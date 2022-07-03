10c3e43d9f6989ce2e21fa30f4bebcee10541ef0
Evan Bay <neopythe@protonmail.ch>
2022-07-03 07:21:40 -0600

    create FeedbackForm component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from './components/Header'
 import FeedbackData from './data/FeedbackData'
+import FeedbackForm from './components/FeedbackForm'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 
@@ -18,6 +19,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
