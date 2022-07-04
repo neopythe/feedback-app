c2519cc1184943224ed1ddb092065e2a6c4385cc
Evan Bay <neopythe@protonmail.ch>
2022-07-03 20:02:44 -0600

    get global state with useContext Hook

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,9 +1,12 @@
-import PropTypes from 'prop-types'
+import { useContext } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 
+import FeedbackContext from '../context/FeedbackContext'
 import FeedbackItem from './FeedbackItem'
 
-function FeedbackList({ feedback, handleDelete }) {
+function FeedbackList({ handleDelete }) {
+  const { feedback } = useContext(FeedbackContext)
+
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>
   }
@@ -36,16 +39,6 @@ function FeedbackList({ feedback, handleDelete }) {
   //     ))}
   //   </div>
   // )
-}
-
-FeedbackList.propTypes = {
-  feedback: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-      rating: PropTypes.number.isRequired,
-    })
-  ),
 }
 
 export default FeedbackList
