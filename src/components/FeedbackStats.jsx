f5d0ef1c8c31a12eb5f1beb3b9392d7fe9c067d2
Evan Bay <neopythe@protonmail.ch>
2022-07-03 21:08:01 -0600

    fix pluralization bug on FeedbackStats component

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -15,7 +15,9 @@ function FeedbackStats() {
 
   return (
     <div className="feedback-stats">
-      <h4>{feedback.length} Reviews</h4>
+      <h4>
+        {feedback.length} Review{feedback.length !== 1 && 's'}
+      </h4>
       <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   )
