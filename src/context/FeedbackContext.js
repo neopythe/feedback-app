7d89d6eb3063e24738ee459ee7e222afd6787587
Evan Bay <neopythe@protonmail.ch>
2022-07-03 21:56:36 -0600

    wire up editFeedback function

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -21,20 +21,36 @@ export const FeedbackProvider = ({ children }) => {
       rating: 7,
     },
   ])
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  })
 
+  // add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
   }
 
+  // delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item) => item.id !== id))
     }
   }
 
+  // set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    })
+  }
+
   return (
-    <FeedbackContext.Provider value={{ feedback, addFeedback, deleteFeedback }}>
+    <FeedbackContext.Provider
+      value={{ feedback, addFeedback, deleteFeedback, editFeedback }}
+    >
       {children}
     </FeedbackContext.Provider>
   )
